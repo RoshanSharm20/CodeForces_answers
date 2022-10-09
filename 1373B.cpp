de8739333e580c3423d79b1d21214938c181c656
roshansharma <roshan.s4d@gmail.com>
2022-10-09 11:28:39 +0530

    main solution added

---
+++ 1373B.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    vector<string> ans;
+    for (int i = 0; i < t; ++i)
+    {
+        string s;
+        cin >> s;
+        stack<char> st;
+        int count = 0;
+        for (int j = 0; j < s.size(); ++j)
+        {
+            if (st.empty())
+                st.push(s[j]);
+            else
+            {
+                if (st.top() != s[j])
+                {
+                    count++;
+                    st.pop();
+                }
+                else
+                    st.push(s[j]);
+            }
+        }
+        if ((count % 2) != 0)
+            ans.push_back("DA");
+        else
+            ans.push_back("NET");
+    }
+    for (int i = 0; i < t; ++i)
+        cout << ans[i] << endl;
+}
